12dbd8c16f66394e958660710111d75d26e12a8c
horup <sah@sah.dk>
2017-06-22 20:23:08 +0200

    fixed out dir

--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var renderer = require("./renderer");
-var Map = require("./map");
-renderer.init(Map.defaultMap);
-//# sourceMappingURL=index.js.map
