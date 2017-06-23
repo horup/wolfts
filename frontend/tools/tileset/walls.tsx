f2eaa5b63e765dd7b51742fe66057d22a5eaef16
horup <sah@sah.dk>
2017-06-23 07:42:51 +0200

    refactored a bit

---
+++ frontend/tools/tileset/walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="walls" tilewidth="64" tileheight="64" tilecount="114" columns="6">
+ <image source="../../dist/textures/walls.png" width="384" height="1216"/>
+</tileset>
